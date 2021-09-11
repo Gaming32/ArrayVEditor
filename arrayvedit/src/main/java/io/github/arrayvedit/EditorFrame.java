a8b17f383c7db6d1972220c424da9e04f4f72cdd
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-09-11 10:08:12 -0500

    Add ability to delete sorts

--- arrayvedit/src/main/java/io/github/arrayvedit/EditorFrame.java
+++ arrayvedit/src/main/java/io/github/arrayvedit/EditorFrame.java
@@ -1,5 +1,7 @@
 package io.github.arrayvedit;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystem;
@@ -12,15 +14,9 @@ import java.util.stream.Collectors;
 
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatLightLaf;
-import com.jthemedetecor.OsThemeDetector;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -28,6 +24,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.jthemedetecor.OsThemeDetector;
 
 public class EditorFrame extends JFrame {
     protected final OsThemeDetector themeDetector;
@@ -52,16 +53,38 @@ public class EditorFrame extends JFrame {
             });
         });
 
-        fileChooser = new JFileChooser();
+        fileChooser = new JFileChooser(System.getProperty("user.dir"));
         fileChooser.setAcceptAllFileFilterUsed(false);
         
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         createComponents();
 
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                if (jarFs != null) {
+                    try {
+                        jarFs.close();
+                    } catch (IOException exc) {
+                        showErrorMessage(exc);
+                        return;
+                    }
+                }
+            }
+        });
+
         pack();
-        // setResizable(false);
+        setTitle("ArrayVEditor");
         setVisible(true);
+    }
+
+    public void showErrorMessage(Throwable e, String title) {
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(EditorFrame.this, e.toString(), title, JOptionPane.ERROR_MESSAGE);
+    }
+
+    public void showErrorMessage(Throwable e) {
+        showErrorMessage(e, getTitle());
     }
 
     protected void createComponents() {
@@ -104,7 +127,12 @@ public class EditorFrame extends JFrame {
         JButton deleteButton = new JButton();
         deleteButton.setText("Delete");
         deleteButton.addActionListener(e -> {
-            // TODO: Delete button
+            int[] sortIndices = sortsList.getSelectedIndices();
+            if (sortIndices.length == 0) {
+                JOptionPane.showMessageDialog(this, "No sorts selected", "Delete Sorts", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            deleteSorts(sortIndices);
         });
 
         GroupLayout layout = new GroupLayout(getContentPane());
@@ -144,8 +172,7 @@ public class EditorFrame extends JFrame {
         try {
             jarFs = FileSystems.newFileSystem(file.toPath());
         } catch (IOException exc) {
-            exc.printStackTrace();
-            JOptionPane.showMessageDialog(this, exc.toString(), "Open JAR", JOptionPane.ERROR_MESSAGE);
+            showErrorMessage(exc, "Open JAR");
             return;
         }
         currentlyLoaded.setText(file.getName());
@@ -162,7 +189,8 @@ public class EditorFrame extends JFrame {
                 sortsListModel.addElement(sort);
             });
         } catch (IOException e) {
-            e.printStackTrace();
+            showErrorMessage(e, "Open JAR");
+            return;
         }
 
         if (sortsListModel.isEmpty()) {
@@ -171,5 +199,47 @@ public class EditorFrame extends JFrame {
 
         sortsList.setEnabled(true);
         pack();
+    }
+
+    protected void deleteSorts(int[] sortIndices) {
+        BasicSortInfo[] sorts = new BasicSortInfo[sortIndices.length];
+        int i = 0;
+        for (int sortIndex : sortIndices) {
+            sorts[i++] = sortsListModel.get(sortIndex);
+        }
+
+        String sortMessage = Arrays.stream(sorts)
+            .map(BasicSortInfo::toString)
+            .collect(Collectors.joining(", "));
+
+        int dialogResult = JOptionPane.showConfirmDialog(this, 
+            String.format(
+                "<html>Are you sure you would like to delete %1$s sorts?<br>" +
+                "%2$s<br>" +
+                "<b>THIS ACTION IS IRREVERSIBLE!</b></html>",
+                sorts.length, sortMessage
+            ),
+            "Delete Sorts", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+
+        if (dialogResult != JOptionPane.YES_OPTION) return;
+
+        i = 0;
+        for (int sortIndex : sortIndices) {
+            sortsListModel.remove(sortIndex - (i++));
+        }
+
+        try {
+            for (BasicSortInfo sort : sorts) {
+                Path fullPath = jarFs.getPath("sorts", sort.id + ".class");
+                Files.delete(fullPath);
+            }
+        } catch (IOException e) {
+            showErrorMessage(e, "Delete Sorts");
+            return;
+        }
+
+        
+
+        JOptionPane.showMessageDialog(this, sorts.length + " sorts deleted", "Delete Sorts", JOptionPane.INFORMATION_MESSAGE);
     }
 }

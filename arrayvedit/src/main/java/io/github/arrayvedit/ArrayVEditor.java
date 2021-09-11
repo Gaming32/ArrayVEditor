f768fe5d162f2f65feffd9156bdcb86e1be16564
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-09-10 19:15:28 -0500

    Begin work on actual GUI

--- arrayvedit/src/main/java/io/github/arrayvedit/ArrayVEditor.java
+++ arrayvedit/src/main/java/io/github/arrayvedit/ArrayVEditor.java
@@ -2,8 +2,28 @@ package io.github.arrayvedit;
 
 import javax.swing.SwingUtilities;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.jthemedetecor.OsThemeDetector;
+
 public class ArrayVEditor {
     public static void main(String[] args) {
+        final OsThemeDetector detector = OsThemeDetector.getDetector();
+        final boolean isDarkThemeUsed = detector.isDark();
+        if (isDarkThemeUsed) {
+            FlatDarkLaf.setup();
+        } else {
+            FlatLightLaf.setup();
+        }
+        detector.registerListener(isDark -> {
+            SwingUtilities.invokeLater(() -> {
+                if (isDark) {
+                    FlatDarkLaf.setup();
+                } else {
+                    FlatLightLaf.setup();
+                }
+            });
+        });
         SwingUtilities.invokeLater(() -> new EditorFrame());
     }
 }
--- arrayvedit/src/main/java/io/github/arrayvedit/EditorFrame.java
+++ arrayvedit/src/main/java/io/github/arrayvedit/EditorFrame.java
@@ -3,44 +3,32 @@ package io.github.arrayvedit;
 import java.awt.GridBagLayout;
 
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatLightLaf;
-import com.jthemedetecor.OsThemeDetector;
 
 public class EditorFrame extends JFrame {
     public EditorFrame() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        final OsThemeDetector detector = OsThemeDetector.getDetector();
-        final boolean isDarkThemeUsed = detector.isDark();
-        if (isDarkThemeUsed) {
-            FlatDarkLaf.setup();
-        } else {
-            FlatLightLaf.setup();
-        }
-        detector.registerListener(isDark -> {
-            SwingUtilities.invokeLater(() -> {
-                if (isDark) {
-                    FlatDarkLaf.setup();
-                } else {
-                    FlatLightLaf.setup();
-                }
-            });
-        });
-
         createComponents();
 
         pack();
+        setResizable(false);
         setVisible(true);
     }
 
     protected void createComponents() {
-        
+        JLabel currentlyLoaded = new JLabel();
+        currentlyLoaded.setText("<No JAR Loaded>");
+
+        JButton loadJARButton = new JButton();
+        loadJARButton.setText("Load JAR");
+        loadJARButton.addActionListener(e -> {
+            
+        });
 
         JPanel mainPanel = new JPanel();
 
@@ -49,9 +37,13 @@ public class EditorFrame extends JFrame {
         layout.setAutoCreateContainerGaps(true);
         layout.setHorizontalGroup(
             layout.createParallelGroup(Alignment.CENTER)
+                .addComponent(currentlyLoaded)
+                .addComponent(loadJARButton)
         );
         layout.setVerticalGroup(
             layout.createSequentialGroup()
+                .addComponent(currentlyLoaded)
+                .addComponent(loadJARButton)
         );
 
         GridBagLayout gbLayout = new GridBagLayout();
